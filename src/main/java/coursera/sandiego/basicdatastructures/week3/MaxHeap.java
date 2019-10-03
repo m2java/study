d0ae71b5bb54f281abb25df7722fc71eb7cc90d1
m2java <>
2019-10-03 18:15:43 +0300

    San Diego. Data Structures. Week3. Heap Done

--- src/main/java/coursera/sandiego/basicdatastructures/week3/MaxHeap.java
+++ src/main/java/coursera/sandiego/basicdatastructures/week3/MaxHeap.java
@@ -1,50 +1,110 @@
 package coursera.sandiego.basicdatastructures.week3;
 
 public class MaxHeap {
-    private int[] arr;
-    private int size;
-    private int capacity;
+  private int[] arr;
+  private int size;
+  private int capacity;
 
-    public MaxHeap(int cap) {
-        this.capacity = cap;
-        arr = new int[capacity];
-        size = 0;
+  public MaxHeap(int cap) {
+    this.capacity = cap;
+    arr = new int[capacity];
+    size = 0;
+  }
+
+  public int getMax() {
+    return arr[0];
+  }
+
+  public void insert(int input) {
+    if (size != capacity) {
+      arr[size] = input;
+      siftUp(size);
+      size++;
     }
+  }
 
-    public int getMax() {
-        return arr[0];
+  public int extractMax() {
+    int result = arr[0];
+    arr[0] = arr[size-1];
+    siftDown(0);
+    size--;
+    return result;
+  }
+
+  private void siftUp(int index) {
+    while (index != 0) {
+      int parentId = (index - 1) / 2;
+      if (arr[index] > arr[parentId]) {
+        int tmp = arr[parentId];
+        arr[parentId] = arr[index];
+        arr[index] = tmp;
+        index = parentId;
+      } else {
+        break;
+      }
     }
+  }
 
-    public void insert(int input) {
-        if (size != capacity) {
-            arr[size] = input;
-            siftUp(size);
-            size++;
+  private void siftDown(int index) {
+    while (index < size) {
+      int leftId = 2 * index + 1;
+      int rightId = 2 * index + 2;
+      if (leftId <= size && arr[index] < arr[leftId] && (
+          arr[index] >= arr[rightId] || rightId > size)) {
+        int tmp = arr[leftId];
+        arr[leftId] = arr[index];
+        arr[index] = tmp;
+        index = leftId;
+      } else if (rightId <= size && arr[index] < arr[rightId] && (
+          arr[index] >= arr[leftId] || leftId > size)) {
+        int tmp = arr[rightId];
+        arr[rightId] = arr[index];
+        arr[index] = tmp;
+        index = rightId;
+      } else if (leftId <= size && rightId <= size && arr[index] < arr[leftId]
+          && arr[index] < arr[rightId]) {
+        int swapId;
+        if (arr[leftId] >= arr[rightId]) {
+          swapId = leftId;
+        } else {
+          swapId = rightId;
         }
+        int tmp = arr[swapId];
+        arr[swapId] = arr[index];
+        arr[index] = tmp;
+        index = swapId;
+      } else {
+        break;
+      }
     }
+  }
 
-    private void siftUp(int index) {
-        while (index != 0) {
-            int parentId = (index-1) / 2;
-            if (arr[index] > arr[parentId]) {
-                int tmp = arr[parentId];
-                arr[parentId] = arr[index];
-                arr[index] = tmp;
-                index = parentId;
-            } else {
-                break;
-            }
-        }
+  public void changePriority(int index, int value) {
+    int oldValue = arr[index];
+    arr[index] = value;
+    if (oldValue < value) {
+      siftUp(index);
+    } else {
+      siftDown(index);
     }
+  }
 
-    public void printHeap() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+  public int remove(int index) {
+    int result = arr[index];
+    arr[index] = Integer.MAX_VALUE;
+    siftUp(index);
+    extractMax();
+    return result;
+  }
 
-    public int getSize() {
-        return size;
+  public void printHeap() {
+    for (int i = 0; i < size; i++) {
+      System.out.print(arr[i] + " ");
     }
+    System.out.println();
+  }
+
+  public int getSize() {
+    return size;
+  }
 }
--- src/test/java/coursera/sandiego/basicdatastructures/week3/MaxHeapTest.java
+++ src/test/java/coursera/sandiego/basicdatastructures/week3/MaxHeapTest.java
@@ -4,16 +4,62 @@ import org.junit.Test;
 
 public class MaxHeapTest {
 
-    @Test
-    public void test() {
-        MaxHeap maxHeap = new MaxHeap(10);
-        System.out.println(maxHeap.getSize());
-        maxHeap.insert(29);
-        maxHeap.insert(18);
-        maxHeap.insert(14);
-        maxHeap.insert(7);
-        maxHeap.insert(42);
-        maxHeap.printHeap();
-        System.out.println(maxHeap.getSize());
-    }
+  @Test
+  public void testInsert() {
+    MaxHeap maxHeap = new MaxHeap(10);
+    System.out.println(maxHeap.getSize());
+    maxHeap.insert(29);
+    maxHeap.insert(18);
+    maxHeap.insert(14);
+    maxHeap.insert(7);
+    maxHeap.insert(42);
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+  }
+
+  @Test
+  public void testExtractMax() {
+    MaxHeap maxHeap = new MaxHeap(10);
+    maxHeap.insert(29);
+    maxHeap.insert(18);
+    maxHeap.insert(14);
+    maxHeap.insert(7);
+    maxHeap.insert(42);
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+    maxHeap.extractMax();
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+  }
+
+  @Test
+  public void testChangePriority() {
+    MaxHeap maxHeap = new MaxHeap(10);
+    maxHeap.insert(29);
+    maxHeap.insert(18);
+    maxHeap.insert(14);
+    maxHeap.insert(7);
+    maxHeap.insert(42);
+    maxHeap.insert(13);
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+    maxHeap.changePriority(2, 50);
+    maxHeap.printHeap();
+  }
+
+  @Test
+  public void testRemove() {
+    MaxHeap maxHeap = new MaxHeap(10);
+    maxHeap.insert(29);
+    maxHeap.insert(18);
+    maxHeap.insert(14);
+    maxHeap.insert(7);
+    maxHeap.insert(42);
+    maxHeap.insert(13);
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+    maxHeap.remove(1);
+    maxHeap.printHeap();
+    System.out.println(maxHeap.getSize());
+  }
 }
